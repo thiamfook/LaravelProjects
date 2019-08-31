7cc6bc37ab0f15dea01a8992a36494e2fbc93815
Jason Thai <thiamfook@gmail.com>
2019-08-31 11:41:52 +0800

    Added images for About page carousel

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -26,12 +26,12 @@ class ProjectController extends Controller
     }
 
     public function edit($id) {
-        $project = Project::find($id);
+        $project = Project::findOrFail($id);
         return view('projects.edit', compact('project'));
     }
 
     public function update($id) {
-        $project = Project::find($id);
+        $project = Project::findOrFail($id);
 
         $project->title = request('title');
         $project->description = request('description');
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,9 +3,30 @@
 @section('title', 'About Us')
 
 @section('content')
-
-    <p>
+  <div id="carouselAbout" class="carousel slide carousel-fade" data-ride="carousel">
+    <ol class="carousel-indicators">
+      @for ($i = 0; $i < 5; $i ++)
+      <li data-target="#carouselAbout" data-slide-to="{{ $i }}"{!! ($i == 0) ? ' class="active"' : '' !!}></li>
+      @endfor
+    </ol>
+    <div class="carousel-inner">
+      @for ($i = 1; $i <= 5; $i ++)
+      <div class="carousel-item{{ ($i == 1) ? ' active' : ''}}">
+        <img src="img/0{{ $i }}.webp" class="d-block w-100" alt="">
+      </div>
+      @endfor
+    </div>
+    <a class="carousel-control-prev" href="#carouselAbout" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselAbout" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus minima eos atque, blanditiis nesciunt impedit assumenda aperiam necessitatibus illum vero fugit quos qui distinctio eum itaque ut? Nam, temporibus voluptatibus?
-    </p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel illo labore, eaque doloribus minus natus commodi deserunt! Ex, dolores dolorem commodi et a aperiam illo consectetur distinctio recusandae incidunt voluptatibus?</p>
+  </p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel illo labore, eaque doloribus minus natus commodi deserunt! Ex, dolores dolorem commodi et a aperiam illo consectetur distinctio recusandae incidunt voluptatibus?</p>
 @endsection
